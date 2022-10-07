007ea53a2512b6fd97c588987dcbae3c783b3c8e
Dhiaeddine Souidi <dhiaeddine.souidi@esprit.tn>
2022-10-07 13:00:25 +0100

    init

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Devqqssssqqqssops!");
+        System.out.println("Hello Devsssqqssssqqqssops!");
     }
 }
